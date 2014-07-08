4fc0b3a7fb608b66d6602cf918c2aaf7298076ef
devudilip <devu.dilip@gmail.com>
2014-07-08 22:22:58 +0530

    Initial commit to Awesome Spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStore::Application.config.secret_token = 'b8e59be5ee4b960533744a95f3b1a9b6b60890c93708b313821543b6ba6eadbee70e9c6310f0cc998b7a80a9b400ca25d7642fb6eceb946daf4bcf26e20e4946'
